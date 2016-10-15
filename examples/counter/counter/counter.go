4550408988b0285134bbd28a2859362c97d05751
Alex Chen <alexanderchenmh@gmail.com>
2016-10-16 04:00:08 +0800

    add readme

--- examples/counter/counter/counter.go
+++ examples/counter/counter/counter.go
@@ -6,6 +6,52 @@ import (
 	"github.com/AlexanderChen1989/tea"
 )
 
+/*
+type Msg enum{
+	None,
+	Incr(int, string),
+	Decr(int),
+}
+
+msg := Msg.None
+msg := Msg.Incr(10, 20)
+msg := Msg.None
+
+match Msg {
+case None:
+case Incr(v1, v2):
+case Decr(v):
+default:
+}
+
+
+type {
+	MsgNone struct {}
+
+	MsgIncr struct {
+		V1 int
+		V2 string
+	}
+
+	Decr struct {
+		V1 int
+	}
+}
+
+var msg interface{} = MsgNone{}
+var msg interface{} = MsgNone{V1: 10, V2: 20}
+
+switch msg := Msg.(type); msg {
+case None:
+case Incr:
+	v1, v2 := msg.V1, msg.V2
+case Decr:
+	v := msg.V1
+default:
+}
+
+*/
+
 type (
 	Model int
 
